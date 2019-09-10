da59bfeac1a1fc46a6e282291e03d8ed4ce1ba67
Drago Odalovic <drago.odalovic@gmail.com>
2019-09-10 14:30:13 +0200

    start writing tests

--- src/components/app/index.test.tsx
+++ src/components/app/index.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import configureMockStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+
 import App from './index'
 
+const mockStore = configureMockStore()
+
 it('renders without crashing', () => {
+  const store = mockStore({ tasks: [], processing: null, filterText: null })
+  const wrappedApp = (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(wrappedApp, div)
   ReactDOM.unmountComponentAtNode(div)
 })
